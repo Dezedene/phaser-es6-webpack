12c1c0fbbd8eed75c63bc9c97135b106015bb0af
Axel <axelchandelier0@gmail.com>
2019-07-11 11:32:46 +0200

    Working

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,27 +1,106 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 
-import Mushroom from '../sprites/Mushroom'
+let player
+let player2
+let cursors
+let keyQ
+let keyZ
+let keyD
 
 export default class extends Phaser.Scene {
   constructor () {
     super({ key: 'GameScene' })
   }
   init () {}
-  preload () {}
+
+  preload () {
+    this.load.image('white', 'asserts/White-Background.png')
+    this.load.image('ground', 'asserts/Rectangle.png')
+    this.load.image('back', 'asserts/Pionk.png')
+    this.load.spritesheet('dude', 'asserts/characters/dude.png', { frameWidth: 32, frameHeight: 48 })
+  }
 
   create () {
-    this.mushroom = new Mushroom({
-      scene: this,
-      x: 400,
-      y: 300,
-      asset: 'mushroom'
+    this.add.image(400, 300, 'white')
+
+    const platforms = this.physics.add.staticGroup()
+
+    platforms.create(400, 500, 'ground')
+    platforms.create(800, 1168, 'back').setScale(3).refreshBody()
+
+    player = this.physics.add.sprite(700, 150, 'dude')
+    player2 = this.physics.add.sprite(100, 150, 'dude')
+
+    player.setBounce(0.2)
+    player.setCollideWorldBounds(true)
+    player2.setBounce(0.2)
+    player2.setCollideWorldBounds(true)
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
     })
 
-    this.add.existing(this.mushroom)
-    this.add.text(100, 100, 'Phaser 3 - ES6 - Webpack ', {
-      font: '64px Bangers',
-      fill: '#7744ff'
+    this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
     })
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    cursors = this.input.keyboard.createCursorKeys()
+    keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q)
+    keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z)
+    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+
+    this.physics.add.collider(player, platforms)
+    this.physics.add.collider(player2, platforms)
+  }
+
+  update () {
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160)
+
+      player.anims.play('left', true)
+    } else if (cursors.right.isDown) {
+      player.setVelocityX(160)
+
+      player.anims.play('right', true)
+    } else {
+      player.setVelocityX(0)
+
+      player.anims.play('turn')
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+      player.setVelocityY(-230)
+    }
+
+    // player 2
+    if (keyQ.isDown) {
+      player2.setVelocityX(-160)
+
+      player2.anims.play('left', true)
+    } else if (keyD.isDown) {
+      player2.setVelocityX(160)
+
+      player2.anims.play('right', true)
+    } else {
+      player2.setVelocityX(0)
+
+      player2.anims.play('turn')
+    }
+    if (keyZ.isDown && player2.body.touching.down) {
+      player2.setVelocityY(-230)
+    }
   }
 }
