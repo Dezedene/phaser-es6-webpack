3cbdd816745480d54c8293109e51626f04876fb2
Axel <axelchandelier0@gmail.com>
2019-07-11 22:09:56 +0200

    m

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,6 +5,8 @@ let ground
 let leftSide
 let rightSide
 let logo
+let logoAccelerationX = 100
+let logoAccelerationY = 100
 let player
 let player2
 let scorePlayer = 0
@@ -15,6 +17,11 @@ let cursors
 let keyQ
 let keyZ
 let keyD
+let Seb
+
+let time = 60
+let timer
+let timedEvent
 
 export default class extends Phaser.Scene {
   constructor () {
@@ -27,11 +34,16 @@ export default class extends Phaser.Scene {
     this.load.image('ground', 'asserts/Right-sideTransparent.png')
     this.load.image('logo', 'asserts/balls/BallTest.png')
     this.load.image('filet', 'asserts/Rectangle2.png')
-    this.load.spritesheet('dude', 'asserts/characters/dude.png', { frameWidth: 32, frameHeight: 48 })
+    this.load.image('Seb', 'asserts/characters/Seb.png')
+    this.load.spritesheet('dude', 'asserts/characters/Lapin.png', { frameWidth: 40, frameHeight: 40 })
   }
 
   create () {
     this.add.image(200, 280, 'sky')
+
+    const dynamic = this.physics.add.group()
+
+    Seb = dynamic.create(400, 0, 'Seb')
 
     ground = this.physics.add.staticGroup()
     leftSide = ground.create(175, 850, 'ground')
@@ -40,12 +52,10 @@ export default class extends Phaser.Scene {
     const filet = this.physics.add.staticGroup()
     filet.create(400, 800, 'filet')
 
-    player = this.physics.add.sprite(700, 150, 'dude')
-    player2 = this.physics.add.sprite(100, 150, 'dude')
+    player = this.physics.add.sprite(700, 350, 'dude')
+    player2 = this.physics.add.sprite(100, 350, 'dude')
 
-    player.setBounce(0.2)
     player.setCollideWorldBounds(true)
-    player2.setBounce(0.2)
     player2.setCollideWorldBounds(true)
 
     const particles = this.add.particles('logo')
@@ -55,11 +65,11 @@ export default class extends Phaser.Scene {
       blendMode: 'ADD'
     })
 
-    logo = this.physics.add.sprite(150, 100, 'logo')
+    logo = this.physics.add.image(150, 100, 'logo')
 
     logo.body.setCircle(20)
-    logo.setVelocity(100, 200)
-    logo.setBounce(1, 1)
+    logo.setVelocity(logoAccelerationX, logoAccelerationY)
+    logo.setBounce(1.2, 1.2)
     logo.setCollideWorldBounds(true)
     this.physics.add.collider(logo, ground)
     this.physics.add.collider(logo, player)
@@ -67,26 +77,6 @@ export default class extends Phaser.Scene {
 
     emitter.setAlpha(0.3)
     emitter.startFollow(logo)
-
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1
-    })
-
-    this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'dude', frame: 4 } ],
-      frameRate: 20
-    })
-
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1
-    })
 
     cursors = this.input.keyboard.createCursorKeys()
     keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q)
@@ -100,24 +90,34 @@ export default class extends Phaser.Scene {
     this.physics.add.collider(filet, player)
     this.physics.add.collider(filet, logo)
 
+    this.physics.add.collider(logo, Seb)
+    this.physics.add.collider(Seb, filet)
+
     scoreText = this.add.text(25, 50, scorePlayer)
     scorePlayer2Text = this.add.text(700, 50, scorePlayer2)
+    timer = this.add.text(400, 50, time)
+
+    const onEvent = () => {
+      time--
+      timer.setText(time)
+    }
+    timedEvent = this.time.addEvent({
+      delay: 1000, // ms
+      callback: onEvent,
+      // args: [],
+      callbackScope: this,
+      loop: true
+    })
   }
 
   update () {
     // player controls
     if (cursors.left.isDown) {
       player.setVelocityX(-200)
-
-      player.anims.play('left', true)
     } else if (cursors.right.isDown) {
       player.setVelocityX(200)
-
-      player.anims.play('right', true)
     } else {
       player.setVelocityX(0)
-
-      player.anims.play('turn')
     }
 
     if (cursors.up.isDown && player.body.touching.down) {
@@ -127,16 +127,10 @@ export default class extends Phaser.Scene {
     // player 2 controls
     if (keyQ.isDown) {
       player2.setVelocityX(-200)
-
-      player2.anims.play('left', true)
     } else if (keyD.isDown) {
       player2.setVelocityX(200)
-
-      player2.anims.play('right', true)
     } else {
       player2.setVelocityX(0)
-
-      player2.anims.play('turn')
     }
     if (keyZ.isDown && player2.body.touching.down) {
       player2.setVelocityY(-230)
@@ -145,18 +139,40 @@ export default class extends Phaser.Scene {
     // ball reset & score handling
 
     if (this.physics.world.collide(logo, rightSide)) {
-      logo.x = 450
-      logo.y = 100
-      logo.setVelocity(100, 200)
+      logo.x = 550
+      logo.y = 300
+      logo.setVelocity(logoAccelerationX, logoAccelerationY)
       scorePlayer += 1
       scoreText.setText(`Score : ${scorePlayer}`)
     }
     if (this.physics.world.collide(logo, leftSide)) {
-      logo.x = 450
-      logo.y = 100
-      logo.setVelocity(100, 200)
+      logo.x = 150
+      logo.y = 300
+      logo.setVelocity(logoAccelerationX, logoAccelerationY)
       scorePlayer2 += 1
       scorePlayer2Text.setText(`Score : ${scorePlayer2}`)
+    }
+
+    if (this.physics.world.collide(Seb, rightSide)) {
+      scorePlayer += 10
+      Seb.destroy()
+      scoreText.setText(`Score : ${scorePlayer}`)
+    }
+    if (this.physics.world.collide(Seb, leftSide)) {
+      scorePlayer2 += 10
+      Seb.destroy()
+      scorePlayer2Text.setText(`Score : ${scorePlayer2}`)
+    }
+
+    if (this.physics.world.collide(logo, player)) {
+      logo.body.setAllowGravity(false)
+    }
+    if (this.physics.world.collide(logo, player2)) {
+      logo.body.setAllowGravity(false)
+    }
+
+    if (logo.y < 250) {
+      logo.body.setAllowGravity(true)
     }
   }
 }
