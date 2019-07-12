43fa935bd2bff54332f5d2dfe9d209eb3d9bdda0
Greg <gregoire.suray@gmail.com>
2019-07-12 06:00:51 +0200

    back button is working

--- src/scenes/OptionScene.js
+++ src/scenes/OptionScene.js
@@ -13,6 +13,7 @@ export default class extends Phaser.Scene {
   preload () {
     this.load.image('soundOnButton', 'asserts/buttons/soundOn.png')
     this.load.image('soundOffButton', 'asserts/buttons/soundOff.png')
+    this.load.image('backButton', 'asserts/buttons/backButton.png')
   }
 
   create () {
@@ -20,12 +21,12 @@ export default class extends Phaser.Scene {
 
     this.add.image(0, 0, 'title_bg').setOrigin(0).setDepth(0)
 
-    const instructionsConf = {
+    const soundInstruction = {
       x: this.game.renderer.width / 3.50,
       y: this.game.renderer.height * 0.61,
       text: 'Sound:',
       style: {
-        fontSize: '40px',
+        fontSize: '35px',
         fontFamily: 'Bangers',
         color: '#ffffff',
         align: 'center',
@@ -33,15 +34,34 @@ export default class extends Phaser.Scene {
       }
     }
 
-    this.make.text(instructionsConf)
+    this.make.text(soundInstruction)
+
+    const backInstruction = {
+      x: this.game.renderer.width / 3.50,
+      y: this.game.renderer.height * 0.86,
+      text: 'Main menu:',
+      style: {
+        fontSize: '35px',
+        fontFamily: 'Bangers',
+        color: '#ffffff',
+        align: 'center',
+        lineSpacing: 44
+      }
+    }
+
+    this.make.text(backInstruction)
 
     let soundOffButton = this.add.image(this.game.renderer.width / 2.20, this.game.renderer.height * 0.65, 'soundOffButton').setDepth(1)
 
     let soundOnButton = this.add.image(this.game.renderer.width / 1.85, this.game.renderer.height * 0.65, 'soundOnButton').setDepth(1)
 
+    let backButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.9, 'backButton').setDepth(1)
+
     soundOnButton.setInteractive()
 
     soundOffButton.setInteractive()
+
+    backButton.setInteractive()
 
     soundOnButton.on('pointerup', () => {
       sound = true
@@ -49,6 +69,10 @@ export default class extends Phaser.Scene {
 
     soundOffButton.on('pointerup', () => {
       sound = false
+    })
+
+    backButton.on('pointerup', () => {
+      this.scene.start('MenuScene')
     })
   }
 
